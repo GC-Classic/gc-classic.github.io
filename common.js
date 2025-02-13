0bc94ccd5a223baf6bdeb31bfafdfa176660cbfc
bbx <bbx>
2025-02-13 09:56:20 +0800

    improv

--- common.js
+++ common.js
@@ -115,27 +115,30 @@ const Menu = () => {
     Menu.events();
 }
 Object.assign(Menu, {
-    align: () => Q('menu').style.left = `-${Math.max(...[...document.querySelectorAll('menu ul')].map(ul => ul.clientWidth))}px`,
-    events: () => Q('menu>li:has(ul)', el => el.onpointerdown = Menu.press),
+    align: () => Q('menu').style.left = `-${Math.max(...[...document.querySelectorAll('menu ol')].map(ol => ol.clientWidth))}px`,
+    events: () => Q('menu>li:has(ol)', el => el.onpointerdown = Menu.press),
     
-    press: ({ target: dragged, clientX: startX }) => {
+    press ({ target: dragged, clientX: startX }) {
         dragged = dragged.closest('li');
         dragged.style.transition = null;
-        let stops = [...dragged.querySelectorAll('li')].reverse().map(li => li.clientWidth).map((sum = 30, w => sum += w));
-        document.onpointermove = document.ontouchmove = ev => Menu.drag(ev, startX, dragged, stops);
+        document.onpointermove = document.ontouchmove = ev => Menu.drag(ev, startX, dragged);
         document.onpointerup = document.ontouchend = ev => Menu.lift(dragged);
     },
-    drag: (ev, startX, dragged, stops) => {
+    drag (ev, startX, dragged) {
         ev.preventDefault();
-        let distance = Math.min((ev.clientX || ev.targetTouches?.[0].pageX) - startX, dragged.Q('ul').clientWidth, stops.at(-1));
-        if (distance <= 0 || !distance) return Q(':checked') && (dragged.Q(':checked').checked = false);
-        dragged.style.transform = `translateX(${distance}px)`;
-        dragged.Q(`li:nth-child(${stops.length - stops.findIndex(w => distance <= w)}) input`).checked = true;
+        let ol = dragged.Q('ol');
+        let stops = [0, ...[...ol.children].reverse().map(li => li.clientWidth)].map((sum => w => sum += w)(32));
+        let distance = Math.min((ev.clientX || ev.targetTouches?.[0].pageX) - startX, stops.at(-1));
+        let checked = stops.findIndex(w => distance <= w);
+        distance && (dragged.style.transform = `translateX(${distance}px)`);
+        distance <= 0 || !distance || !checked ? 
+            [ol.Q('input')].flat().forEach(input => input.checked = false) :
+            [...ol.children].at(checked*-1).Q(`input`).checked = true;
     },
-    lift: dragged => {
+    lift (dragged) {
         document.onpointermove = document.ontouchmove = document.onpointerup = document.ontouchend = dragged.style.transform = null;
         dragged.style.transition = 'transform .5s';
-        Menu.action?.(dragged);
+        dragged.Q('ol :checked') && Menu.action?.(dragged);
     },
 });
 class Stats {
@@ -174,10 +177,10 @@ class Stats {
         .add(...Object.values(equipped ?? Runes.equipped()).map(rune => rune.rune.stats))
         .add(...Runes.equipped.sets().map(s => Rune.set.effect[s]));
     static baseNrune = equipped => Stats.runes(equipped, true).TA;
-    static decimals = ['CAC','CAD','HP','MP','CD','TR','CAR','GP','HSC','BAD'];
+    static decimals = ['CAC','CAD','HP','MP','CR','TR','CAR','GP','HSC','BAD'];
     static round = ({prop, value}) => Stats.decimals.includes(prop) ? value.toFixed(2) : value.toFixed(0);
-    static sample = {A:15000,HP:30,D:8000,MP:50,V:8000,GP:1,SA:8000,HS:24,SD:900,CAR:30,CAC:50,TR:12,CAD:400,CD:30}
-    static order = ['A','D','CAC','V','CAD','CAR','SA','SD','MP','HP','HSC','CD','HS','GP','TR','BAD']
+    static sample = {A:15000,HP:30,D:8000,MP:50,V:8000,GP:1,SA:8000,HS:24,SD:900,CAR:30,CAC:50,TR:12,CAD:400,CR:30}
+    static order = ['A','D','CAC','V','CAD','CAR','SA','SD','MP','HP','HSC','CR','HS','GP','TR','BAD']
 }
 class Icon extends HTMLElement {
     constructor() {
@@ -224,9 +227,9 @@ class Icon extends HTMLElement {
         this.sQ('img').src = Icon.others[this.getAttribute('prop')] ? 
             `/rune/set/${Icon.others[this.getAttribute('prop')]}.webp` : `/property.jpg`;
     }
-    static sequence = ['A','D','V','TR','SA','CAC','CAD','MP','HP','CD','CAR','SD','HS','GP'];
-    static en = {A:'Attack',D:'Defense',V:'Vitality',SA:'Special attack',SD:'Special defense',CAC:'Critical attack chance',CAD:'Critical attack damage',CAR:'Critical attack resistance',HP:'HP recovery',MP:'MP recovery',TR:'Taint resistance',CD:'Counter defense',GP:'GP gain',HS:'Hell spear',HSC:'Hell spear chance',BAD:'Back attack damage'};
-    static zh = {A:'攻擊力',D:'防禦力',V:'生命力',SA:'必殺攻擊力',SD:'必殺防禦力',CAC:'暴擊率',CAD:'暴擊傷害',CAR:'暴擊抵抗',HP:'HP 回復',MP:'MP 回復',TR:'侵蝕抵抗',CD:'克制抵抗',GP:'GP 獲得',HS:'地獄之矛',HSC:'地獄之矛率',BAD:'背擊傷害'};
+    static sequence = ['A','D','V','TR','SA','CAC','CAD','MP','HP','CR','CAR','SD','HS','GP'];
+    static en = {A:'Attack',D:'Defense',V:'Vitality',SA:'Special attack',SD:'Special defense',CAC:'Critical attack/strike chance',CAD:'Critical attack/strike damage',CAR:'Critical attack/strike resistance',HP:'HP recovery',MP:'MP recovery',TR:'Taint resistance',CR:'Counter resistance/defense',GP:'GP gain',HS:'Hell spear (damage)',HSC:'Hell spear chance',BAD:'Back attack damage'};
+    static zh = {A:'攻擊力',D:'防禦力',V:'生命力',SA:'必殺攻擊力',SD:'必殺防禦力',CAC:'暴擊率',CAD:'暴擊傷害',CAR:'暴擊抵抗',HP:'HP 回復',MP:'MP 回復',TR:'侵蝕抵抗',CR:'克制抵抗',GP:'GP 獲得',HS:'地獄之矛 (傷害)',HSC:'地獄之矛率',BAD:'背擊傷害'};
     static others = {HSC:'javelin', HS:'roar', CAD:'doom', HP:'recovery', TR:'resist',EXP:'grow',BAD:'fight'};
     static observedAttributes = ['tips'];
     get prop() {return this.getAttribute('prop');}
